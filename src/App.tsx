e39b28f89fa42cfeaf51cfb53c8bacc90d86947a
Moisés <moisesnetored@gmail.com>
2022-06-22 11:04:14 -0400

    feat: Apollo Client added and configured.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 export function App() {
+  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSON_QUERY);
+
+  console.log(data);
+
   return (
-    <h1 className="text-2xl font-bold text-violet-500">Hello World</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
