84432a5a549a38b903c23d0f00a5698ea08ff907
Moisés Neto <moisesnetored@gmail.com>
2022-06-22 22:42:32 -0400

    wip: Sidebar being created, Lesson in progress.

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,39 @@
-export function Lesson(){
+import { CheckCircle } from 'phosphor-react';
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps){
+  const isLessonAvailable = false;
+
   return(
-    <h1>Lesson</h1>
+    <a href="#">
+      <span className="text-gray-300">
+        {props.availableAt.toString()}
+      </span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <CheckCircle size={20} /> 
+            Conteúdo liberado</span>
+          ): (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} /> 
+              Em breve
+            </span>
+          )}
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">
+          {props.title}
+        </strong>
+      </div>
+    </a>
   );
 }
